90de3aaea2ec25ddb9200a3d4a70381dbba000ac
Simon Swanson <sswanson@palantir.com>
2017-05-02 12:56:34 -0700

    clipboard works with vim compile

--- setup_vim.sh
+++ setup_vim.sh
@@ -11,21 +11,21 @@ runConfigureCommand () {
     local COMMAND="./configure"
     COMMAND="${COMMAND} --with-features=huge"
     COMMAND="${COMMAND} --with-x"
-    COMMAND="${COMMAND} --disable-gui"
+    COMMAND="${COMMAND} --enable-gui"
     COMMAND="${COMMAND} --enable-multibyte"
     COMMAND="${COMMAND} --enable-rubyinterp=yes"
     COMMAND="${COMMAND} --enable-python3interp=yes"
-    COMMAND="${COMMAND} --with-python3-config-dir=/usr/lib/python3.5/config-3.5m-x86_64-linux-gnu/"
+    COMMAND="${COMMAND} --with-python3-config-dir=/usr/lib/python3.5/config-3.5m-x86_64-linux-gnu"
     echo "Running '${COMMAND}'..."
     eval $COMMAND
 }
 
 if [ "$(uname)" == "Linux" ]; then
+    echo "Making sure prerequisites are installed"
     sudo apt-get update
-    sudo apt-get build-dep vim
-    sudo apt-get install x11-dev dbus-x11
-    sudo -k
+    sudo apt-get install xorg-dev python3-dev
 fi
 
 runConfigureCommand && make && sudo make install
-echo "Exited with status $?"
+sudo -k
+

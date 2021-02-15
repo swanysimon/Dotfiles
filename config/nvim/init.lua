c87c31c977297a6db07b743a2e83bf4c4018e0a8
Simon Swanson <nomiswanson@gmail.com>
2021-02-14 16:17:27 -0800

    Add lsp config
    Switch to gruvbox community

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -18,7 +18,7 @@ append("o", "shortmess", "c", "")
 set("o", "hidden", true)
 set("o", "lazyredraw", true)
 set("o", "timeoutlen", 200)
-set("o", "updatetime", 300)
+set("o", "updatetime", 100)
 
 
 -- appearance
@@ -26,8 +26,11 @@ set("o", "background", "dark")
 set("o", "laststatus", 2)
 set("o", "showtabline", 2)
 set("o", "termguicolors", true)
+set("o", "title", true)
 set("wo", "colorcolumn", "+1")
 set("wo", "cursorline", true)
+set("wo", "list", true)
+set("wo", "listchars", "trail:»,tab:»-")
 set("wo", "number", true)
 set("wo", "relativenumber", true)
 set("wo", "signcolumn", "auto")
@@ -57,6 +60,8 @@ augroup({
 -- search settings
 set("o", "history", 1000)
 set("o", "ignorecase", true)
+set("o", "incsearch", true)
+set("o", "smartcase", true)
 set("o", "wildignorecase", true)
 
 
@@ -65,6 +70,7 @@ local default_indentation = 2
 
 set("o", "joinspaces", false)
 set("bo", "comments", "")
+set("bo", "copyindent", true)
 set("bo", "expandtab", true)
 set("bo", "shiftwidth", default_indentation)
 set("bo", "softtabstop", default_indentation)
@@ -80,6 +86,7 @@ set("o", "splitright", true)
 
 -- toggles
 map("n", "coh", ":set hlsearch! hlsearch?<CR>")
+map("n", "cos", ":setlocal list! list?<CR>")
 map("n", "cos", ":setlocal spell! spell?<CR>")
 map("n", "cow", ":setlocal wrap! wrap?<CR>")
 
@@ -100,3 +107,7 @@ map("n", "<C-h>", "<C-w>h")
 map("n", "<C-j>", "<C-w>j")
 map("n", "<C-k>", "<C-w>k")
 map("n", "<C-l>", "<C-w>l")
+
+
+-- language server
+require("lsp").init()

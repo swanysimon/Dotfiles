c856b463eac3752283903c82435b6d64958ae84c
Simon Swanson <nomiswanson@gmail.com>
2021-02-17 12:53:10 -0800

    Visual changes to Alacritty and Lualine

--- config/nvim/lua/lualineconfig.lua
+++ config/nvim/lua/lualineconfig.lua
@@ -3,6 +3,8 @@ local M = {}
 function M.sync_lualine()
   local lualine = require("lualine")
   lualine.theme = vim.g.colors_name
+  -- TODO: detect if a patched font is active
+  lualine.options.icons_enabled = false
   lualine.status()
 end
 

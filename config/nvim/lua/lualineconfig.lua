4cc977da6b3f3b91663fedb3f22b809c63ee28e9
Simon Swanson <nomiswanson@gmail.com>
2021-04-06 13:52:27 -0700

    Update submodules
    Breaking change in lualine configuration

--- config/nvim/lua/lualineconfig.lua
+++ config/nvim/lua/lualineconfig.lua
@@ -1,11 +1,13 @@
 local M = {}
 
 function M.sync_lualine()
-  local lualine = require("lualine")
-  lualine.theme = vim.g.colors_name
-  -- TODO: detect if a patched font is active
-  lualine.options.icons_enabled = false
-  lualine.status()
+  require("lualine").setup {
+    options = {
+      -- TODO: detect if a patched font is active
+      icons_enabled = false,
+      theme = vim.g.colors_name,
+    },
+  }
 end
 
 return M

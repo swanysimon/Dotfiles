39a131dbad3584253fa0346c35e9474ceec55687
Simon Swanson <gitlab@simonswanson.xyz>
2021-11-28 23:52:14 -0800

    Add actual installation instructions
    Add instructions for getting a new machine set up with Homebrew, Neovim,
    and the fish shell. Still TODO is instructions for sane SSH and GPG key
    management.

--- README.md
+++ README.md
@@ -1,13 +1,60 @@
 # Simon's Dotfiles
 
 This repository is currently under construction. More documentation is coming.
+Maybe.
 
-Do not copy-paste sections of this codebase! It is not designed for others to
+> Do not copy-paste blindly from this codebase! It is not designed for others to
 use, but may be useful to you setting up your system.
-
----
 
 ## Installation
 
-The `config` directory should be symlinked to the `XDG_CONFIG_HOME` environment
-variable. Most tools know to look in there for configuration options.
+ 1. Clone this repository.
+
+    TODO: instructions for creating SSH keys and configuring them.
+
+    ```shell
+    git clone git@gitlab.com:swanysimon/dotfiles.git
+    cd dotfiles
+    ```
+
+ 1. Symlink the `config` directory to the place you want `XDG_CONFIG_HOME` to
+    be. This is normally `$HOME/.config`:
+
+    ```shell
+    cd
+    rm -r .config
+    ln -si <dotfiles repository>/config .config
+    ```
+
+ 1. Install [Homebrew] and related utilities. Homebrew is my MacOS package
+    manager of choice. Once installed, install all the utilities I use
+    regularly:
+
+    ```shell
+    brew bundle install --cleanup --force
+    brew cleanup -s --prune 0
+    brew doctor
+    ```
+
+    For non-personal machines, set up a private dotfiles repository with its own
+    Brewfile setup instead. The utilities installed here are often very
+    unrelated to anything remotely associated with a job. No employer needs to
+    run a security scan on Slippi.
+
+ 1. Configure [fish] as the default shell. You can then close your shell and
+    open [Alacritty] (my terminal of choice for now) whenever you like:
+
+    ```shell
+    chsh -s "$(brew --prefix)/bin/fish"
+    ```
+
+ 1. Initialize [Neovim] and all its plugins:
+
+    ```shell
+    nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
+    ```
+
+[Alacritty]: https://alacritty.org
+[fish]: https://fishshell.com
+[Homebrew]: https://brew.sh
+[Neovim]: https://neovim.io
--- config/nvim/plugin/packer_compiled.lua
+++ config/nvim/plugin/packer_compiled.lua
@@ -72,7 +72,8 @@ _G.packer_plugins = {
   ["FTerm.nvim"] = {
     config = { "\27LJ\2\n3\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0\24plugin.config.fterm\frequire\0" },
     loaded = true,
-    path = "/Users/simon/.local/share/nvim/site/pack/packer/start/FTerm.nvim"
+    path = "/Users/simon/.local/share/nvim/site/pack/packer/start/FTerm.nvim",
+    url = "https://github.com/numtostr/FTerm.nvim"
   },
   ["cmp-buffer"] = {
     after_files = { "/Users/simon/.local/share/nvim/site/pack/packer/opt/cmp-buffer/after/plugin/cmp_buffer.lua" },
@@ -81,7 +82,8 @@ _G.packer_plugins = {
     },
     loaded = false,
     needs_bufread = false,
-    path = "/Users/simon/.local/share/nvim/site/pack/packer/opt/cmp-buffer"
+    path = "/Users/simon/.local/share/nvim/site/pack/packer/opt/cmp-buffer",
+    url = "https://github.com/hrsh7th/cmp-buffer"
   },
   ["cmp-cmdline"] = {
     after_files = { "/Users/simon/.local/share/nvim/site/pack/packer/opt/cmp-cmdline/after/plugin/cmp_cmdline.lua" },
@@ -90,7 +92,8 @@ _G.packer_plugins = {
     },
     loaded = false,
     needs_bufread = false,
-    path = "/Users/simon/.local/share/nvim/site/pack/packer/opt/cmp-cmdline"
+    path = "/Users/simon/.local/share/nvim/site/pack/packer/opt/cmp-cmdline",
+    url = "https://github.com/hrsh7th/cmp-cmdline"
   },
   ["cmp-nvim-lsp"] = {
     after_files = { "/Users/simon/.local/share/nvim/site/pack/packer/opt/cmp-nvim-lsp/after/plugin/cmp_nvim_lsp.lua" },
@@ -99,7 +102,8 @@ _G.packer_plugins = {
     },
     loaded = false,
     needs_bufread = false,
-    path = "/Users/simon/.local/share/nvim/site/pack/packer/opt/cmp-nvim-lsp"
+    path = "/Users/simon/.local/share/nvim/site/pack/packer/opt/cmp-nvim-lsp",
+    url = "https://github.com/hrsh7th/cmp-nvim-lsp"
   },
   ["cmp-nvim-lua"] = {
     after_files = { "/Users/simon/.local/share/nvim/site/pack/packer/opt/cmp-nvim-lua/after/plugin/cmp_nvim_lua.lua" },
@@ -108,7 +112,8 @@ _G.packer_plugins = {
     },
     loaded = false,
     needs_bufread = false,
-    path = "/Users/simon/.local/share/nvim/site/pack/packer/opt/cmp-nvim-lua"
+    path = "/Users/simon/.local/share/nvim/site/pack/packer/opt/cmp-nvim-lua",
+    url = "https://github.com/hrsh7th/cmp-nvim-lua"
   },
   ["cmp-path"] = {
     after_files = { "/Users/simon/.local/share/nvim/site/pack/packer/opt/cmp-path/after/plugin/cmp_path.lua" },
@@ -117,39 +122,46 @@ _G.packer_plugins = {
     },
     loaded = false,
     needs_bufread = false,
-    path = "/Users/simon/.local/share/nvim/site/pack/packer/opt/cmp-path"
+    path = "/Users/simon/.local/share/nvim/site/pack/packer/opt/cmp-path",
+    url = "https://github.com/hrsh7th/cmp-path"
   },
   ["gruvbox.nvim"] = {
     config = { "\27LJ\2\n[\0\0\3\0\6\0\t6\0\0\0009\0\1\0'\1\3\0=\1\2\0006\0\0\0009\0\4\0'\2\5\0B\0\2\1K\0\1\0\24colorscheme gruvbox\bcmd\tdark\15background\bopt\bvim\0" },
     loaded = true,
-    path = "/Users/simon/.local/share/nvim/site/pack/packer/start/gruvbox.nvim"
+    path = "/Users/simon/.local/share/nvim/site/pack/packer/start/gruvbox.nvim",
+    url = "https://github.com/ellisonleao/gruvbox.nvim"
   },
   ["lsp_signature.nvim"] = {
     load_after = {},
     loaded = true,
     needs_bufread = false,
-    path = "/Users/simon/.local/share/nvim/site/pack/packer/opt/lsp_signature.nvim"
+    path = "/Users/simon/.local/share/nvim/site/pack/packer/opt/lsp_signature.nvim",
+    url = "https://github.com/ray-x/lsp_signature.nvim"
   },
   ["lush.nvim"] = {
     loaded = true,
-    path = "/Users/simon/.local/share/nvim/site/pack/packer/start/lush.nvim"
+    path = "/Users/simon/.local/share/nvim/site/pack/packer/start/lush.nvim",
+    url = "https://github.com/rktjmp/lush.nvim"
   },
   ["nvim-bufdel"] = {
     loaded = true,
-    path = "/Users/simon/.local/share/nvim/site/pack/packer/start/nvim-bufdel"
+    path = "/Users/simon/.local/share/nvim/site/pack/packer/start/nvim-bufdel",
+    url = "https://github.com/ojroques/nvim-bufdel"
   },
   ["nvim-cmp"] = {
-    after = { "cmp-buffer", "cmp-nvim-lua", "cmp-path", "cmp-cmdline", "cmp-nvim-lsp" },
+    after = { "cmp-cmdline", "cmp-nvim-lua", "cmp-path", "cmp-nvim-lsp", "cmp-buffer" },
     config = { "\27LJ\2\n1\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0\22plugin.config.cmp\frequire\0" },
     loaded = false,
     needs_bufread = false,
     only_cond = false,
-    path = "/Users/simon/.local/share/nvim/site/pack/packer/opt/nvim-cmp"
+    path = "/Users/simon/.local/share/nvim/site/pack/packer/opt/nvim-cmp",
+    url = "https://github.com/hrsh7th/nvim-cmp"
   },
   ["nvim-colorizer.lua"] = {
     config = { "\27LJ\2\n7\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\14colorizer\frequire\0" },
     loaded = true,
-    path = "/Users/simon/.local/share/nvim/site/pack/packer/start/nvim-colorizer.lua"
+    path = "/Users/simon/.local/share/nvim/site/pack/packer/start/nvim-colorizer.lua",
+    url = "https://github.com/norcalli/nvim-colorizer.lua"
   },
   ["nvim-lspconfig"] = {
     after = { "lsp_signature.nvim" },
@@ -159,86 +171,97 @@ _G.packer_plugins = {
   ["nvim-treesitter"] = {
     config = { "\27LJ\2\n8\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0\29plugin.config.treesitter\frequire\0" },
     loaded = true,
-    path = "/Users/simon/.local/share/nvim/site/pack/packer/start/nvim-treesitter"
+    path = "/Users/simon/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
+    url = "https://github.com/nvim-treesitter/nvim-treesitter"
   },
   ["packer.nvim"] = {
     loaded = true,
-    path = "/Users/simon/.local/share/nvim/site/pack/packer/start/packer.nvim"
+    path = "/Users/simon/.local/share/nvim/site/pack/packer/start/packer.nvim",
+    url = "https://github.com/wbthomason/packer.nvim"
   },
   ["plenary.nvim"] = {
     loaded = true,
-    path = "/Users/simon/.local/share/nvim/site/pack/packer/start/plenary.nvim"
+    path = "/Users/simon/.local/share/nvim/site/pack/packer/start/plenary.nvim",
+    url = "https://github.com/nvim-lua/plenary.nvim"
   },
   ["spellsitter.nvim"] = {
     loaded = true,
-    path = "/Users/simon/.local/share/nvim/site/pack/packer/start/spellsitter.nvim"
+    path = "/Users/simon/.local/share/nvim/site/pack/packer/start/spellsitter.nvim",
+    url = "https://github.com/lewis6991/spellsitter.nvim"
   },
   ["telescope.nvim"] = {
     config = { "\27LJ\2\n7\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0\28plugin.config.telescope\frequire\0" },
     loaded = true,
-    path = "/Users/simon/.local/share/nvim/site/pack/packer/start/telescope.nvim"
+    path = "/Users/simon/.local/share/nvim/site/pack/packer/start/telescope.nvim",
+    url = "https://github.com/nvim-telescope/telescope.nvim"
   },
   ["trouble.nvim"] = {
     config = { "\27LJ\2\n5\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\ftrouble\frequire\0" },
     loaded = true,
-    path = "/Users/simon/.local/share/nvim/site/pack/packer/start/trouble.nvim"
+    path = "/Users/simon/.local/share/nvim/site/pack/packer/start/trouble.nvim",
+    url = "https://github.com/folke/trouble.nvim"
   },
   ["vim-commentary"] = {
     loaded = true,
-    path = "/Users/simon/.local/share/nvim/site/pack/packer/start/vim-commentary"
+    path = "/Users/simon/.local/share/nvim/site/pack/packer/start/vim-commentary",
+    url = "https://github.com/tpope/vim-commentary"
   },
   ["vim-matchup"] = {
     loaded = true,
-    path = "/Users/simon/.local/share/nvim/site/pack/packer/start/vim-matchup"
+    path = "/Users/simon/.local/share/nvim/site/pack/packer/start/vim-matchup",
+    url = "https://github.com/andymass/vim-matchup"
   },
   ["vim-repeat"] = {
     loaded = true,
-    path = "/Users/simon/.local/share/nvim/site/pack/packer/start/vim-repeat"
+    path = "/Users/simon/.local/share/nvim/site/pack/packer/start/vim-repeat",
+    url = "https://github.com/tpope/vim-repeat"
   },
   ["vim-startify"] = {
     config = { "\27LJ\2\n6\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0\27plugin.config.startify\frequire\0" },
     loaded = true,
-    path = "/Users/simon/.local/share/nvim/site/pack/packer/start/vim-startify"
+    path = "/Users/simon/.local/share/nvim/site/pack/packer/start/vim-startify",
+    url = "https://github.com/mhinz/vim-startify"
   },
   ["vim-surround"] = {
     loaded = true,
-    path = "/Users/simon/.local/share/nvim/site/pack/packer/start/vim-surround"
+    path = "/Users/simon/.local/share/nvim/site/pack/packer/start/vim-surround",
+    url = "https://github.com/tpope/vim-surround"
   }
 }
 
 time([[Defining packer_plugins]], false)
--- Config for: nvim-lspconfig
-time([[Config for nvim-lspconfig]], true)
-try_loadstring("\27LJ\2\n7\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0\28plugin.config.lspconfig\frequire\0", "config", "nvim-lspconfig")
-time([[Config for nvim-lspconfig]], false)
--- Config for: vim-startify
-time([[Config for vim-startify]], true)
-try_loadstring("\27LJ\2\n6\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0\27plugin.config.startify\frequire\0", "config", "vim-startify")
-time([[Config for vim-startify]], false)
--- Config for: trouble.nvim
-time([[Config for trouble.nvim]], true)
-try_loadstring("\27LJ\2\n5\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\ftrouble\frequire\0", "config", "trouble.nvim")
-time([[Config for trouble.nvim]], false)
--- Config for: FTerm.nvim
-time([[Config for FTerm.nvim]], true)
-try_loadstring("\27LJ\2\n3\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0\24plugin.config.fterm\frequire\0", "config", "FTerm.nvim")
-time([[Config for FTerm.nvim]], false)
--- Config for: telescope.nvim
-time([[Config for telescope.nvim]], true)
-try_loadstring("\27LJ\2\n7\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0\28plugin.config.telescope\frequire\0", "config", "telescope.nvim")
-time([[Config for telescope.nvim]], false)
--- Config for: nvim-treesitter
-time([[Config for nvim-treesitter]], true)
-try_loadstring("\27LJ\2\n8\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0\29plugin.config.treesitter\frequire\0", "config", "nvim-treesitter")
-time([[Config for nvim-treesitter]], false)
 -- Config for: gruvbox.nvim
 time([[Config for gruvbox.nvim]], true)
 try_loadstring("\27LJ\2\n[\0\0\3\0\6\0\t6\0\0\0009\0\1\0'\1\3\0=\1\2\0006\0\0\0009\0\4\0'\2\5\0B\0\2\1K\0\1\0\24colorscheme gruvbox\bcmd\tdark\15background\bopt\bvim\0", "config", "gruvbox.nvim")
 time([[Config for gruvbox.nvim]], false)
+-- Config for: nvim-treesitter
+time([[Config for nvim-treesitter]], true)
+try_loadstring("\27LJ\2\n8\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0\29plugin.config.treesitter\frequire\0", "config", "nvim-treesitter")
+time([[Config for nvim-treesitter]], false)
+-- Config for: FTerm.nvim
+time([[Config for FTerm.nvim]], true)
+try_loadstring("\27LJ\2\n3\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0\24plugin.config.fterm\frequire\0", "config", "FTerm.nvim")
+time([[Config for FTerm.nvim]], false)
 -- Config for: nvim-colorizer.lua
 time([[Config for nvim-colorizer.lua]], true)
 try_loadstring("\27LJ\2\n7\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\14colorizer\frequire\0", "config", "nvim-colorizer.lua")
 time([[Config for nvim-colorizer.lua]], false)
+-- Config for: vim-startify
+time([[Config for vim-startify]], true)
+try_loadstring("\27LJ\2\n6\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0\27plugin.config.startify\frequire\0", "config", "vim-startify")
+time([[Config for vim-startify]], false)
+-- Config for: nvim-lspconfig
+time([[Config for nvim-lspconfig]], true)
+try_loadstring("\27LJ\2\n7\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0\28plugin.config.lspconfig\frequire\0", "config", "nvim-lspconfig")
+time([[Config for nvim-lspconfig]], false)
+-- Config for: telescope.nvim
+time([[Config for telescope.nvim]], true)
+try_loadstring("\27LJ\2\n7\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0\28plugin.config.telescope\frequire\0", "config", "telescope.nvim")
+time([[Config for telescope.nvim]], false)
+-- Config for: trouble.nvim
+time([[Config for trouble.nvim]], true)
+try_loadstring("\27LJ\2\n5\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\ftrouble\frequire\0", "config", "trouble.nvim")
+time([[Config for trouble.nvim]], false)
 -- Load plugins in order defined by `after`
 time([[Sequenced loading]], true)
 vim.cmd [[ packadd lsp_signature.nvim ]]

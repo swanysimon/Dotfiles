fe726da3c85d442873eff122c243d2e0d7257d31
Simon Swanson <gitlab@simonswanson.xyz>
2022-01-05 11:23:34 -0800

    Fix startify lists labeling
    Sessions were not named correctly.
    Commands seem to not do anything.
    Added bookmarks section because it doesn't appear if there aren't any.

--- config/nvim/lua/plugin/config/startify.lua
+++ config/nvim/lua/plugin/config/startify.lua
@@ -2,10 +2,10 @@ local g = vim.g
 
 
 g.startify_lists = {
-  {type = "sessions"},
-  {type = "dir", header = {"   Recents " .. require("os").getenv("PWD")}},
+  {type = "sessions", header = {"   Sessions"}},
+  {type = "bookmarks", header = {"   Bookmarks"}},
+  {type = "dir", header = {"   Recents in " .. require("os").getenv("PWD")}},
   {type = "files", header = {"   Recents"}},
-  {type = "commands", header = {"   Commands"}}
 }
 g.startify_session_autoload = true
 g.startify_session_persistence = true

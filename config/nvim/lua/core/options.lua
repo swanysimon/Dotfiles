607a810aefed68fb88a49c567e07902abcfc56e4
Simon Swanson <gitlab@simonswanson.xyz>
2022-01-05 11:19:08 -0800

    Use array instead of comma-separated string for completeopt
    Using Lua types when possible in the vim configuration

--- config/nvim/lua/core/options.lua
+++ config/nvim/lua/core/options.lua
@@ -31,7 +31,7 @@ opt.termguicolors = true
 
 -- editing
 opt.breakindent = true
-opt.completeopt = "menuone,noselect"  -- necessary for completion engine
+opt.completeopt = {"menu", "menuone", "noselect"}  -- necessary for completion engine
 opt.expandtab = true
 opt.shiftround = true
 opt.shiftwidth = 2

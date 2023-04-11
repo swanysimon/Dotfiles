71eb847700184c84c00f77554b84a6c999c53f14
Simon Swanson <gitlab@simonswanson.xyz>
2023-04-10 19:03:37 -0700

    Remove spellsitter
    This functionality has apparently been merged into Neovim itself, and
    the plugin was causing errors when opening up commit messages. Used this
    opportunity to inline the Treesitter configuration

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -78,12 +78,34 @@ local plugins = {
   -- treesitter
   {
     "nvim-treesitter/nvim-treesitter",
-    requires = {
-      "andymass/vim-matchup",
-      "lewis6991/spellsitter.nvim",
-    },
+    requires = "andymass/vim-matchup",
     config = function()
-      require("plugins.treesitter")
+      require("nvim-treesitter.configs").setup {
+        ensure_installed = {
+          "bash",
+          "comment",
+          "dockerfile",
+          "fish",
+          "git_rebase",
+          "gitattributes",
+          "gitcommit",
+          "gitignore",
+          "help",
+          "java",
+          "json",
+          "lua",
+          "make",
+          "python",
+          "rst",
+          "rust",
+          "toml",
+          "vim",
+          "yaml",
+        },
+        highlight = {enable = true},
+        indent = {enable = true},
+        matchup = {enable = true},
+      }
     end,
     run = ":TSUpdate",
   },

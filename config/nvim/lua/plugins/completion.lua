dc94e004ec3e430c9ed2797d9626267ecbce878c
Simon Swanson <gitlab@simonswanson.xyz>
2024-02-07 17:25:07 -0800

    Fix completion

--- config/nvim/lua/plugins/completion.lua
+++ config/nvim/lua/plugins/completion.lua
@@ -2,43 +2,33 @@ local function cycle(forward)
   local cmp = require("cmp")
   local luasnip = require("luasnip")
 
-  local function cycle_internal(use_snippets)
-    return function(fallback)
-      local invisible = not cmp.visible()
-      local try_snippets = invisible and use_snippets
-
-      if try_snippets and forward then
-        local col = vim.fn.col('.') - 1
-        if luasnip.expand_or_locally_jumpable() then
-          luasnip.expand_or_jump()
-        elseif col == 0 or vim.fn.getline("."):sub(col, col):match("%s") then
-          fallback()
-        else
-          cmp.complete()
-        end
-      elseif try_snippets then
-        if luasnip.jumpable(-1) then
-          luasnip.jump(-1)
-        else
-          fallback()
-        end
-      elseif invisible then
+  local cycle_internal = function(fallback)
+    local invisible = not cmp.visible()
+    if invisible and forward then
+      local col = vim.fn.col('.') - 1
+      if luasnip.expand_or_locally_jumpable() then
+        luasnip.expand_or_jump()
+      elseif col == 0 or vim.fn.getline("."):sub(col, col):match("%s") then
         fallback()
-      elseif #cmp.get_entries() == 1 then
-        cmp.confirm({select = true})
-      elseif forward then
-        cmp.select_next_item({behavior = cmp.SelectBehavior.Select})
       else
-        cmp.select_prev_item({behavior = cmp.SelectBehavior.Select})
+        cmp.complete()
       end
+    elseif invisible then
+      if luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    elseif #cmp.get_entries() == 1 then
+      cmp.confirm({select = true})
+    elseif forward then
+      cmp.select_next_item({behavior = cmp.SelectBehavior.Select})
+    else
+      cmp.select_prev_item({behavior = cmp.SelectBehavior.Select})
     end
   end
 
-  return require("cmp").mapping({
-    c = cycle_internal(false),
-    i = cycle_internal(true),
-    s = cycle_internal(true),
-  })
+  return cmp.mapping(cycle_internal, { "i", "s", })
 end
 
 

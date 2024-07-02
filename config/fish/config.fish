a17d0b5c37586172cbbcf7c334ee0813221cb1be
Simon Swanson <gitlab@simonswanson.xyz>
2024-07-02 10:13:13 -0700

    Add mise configuration

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -57,63 +57,13 @@ if type -p mise &>/dev/null
 end
 
 
-if type -p nvm &>/dev/null
-    set -gx nvm_default_version system
-
-    function __nvm_auto --on-variable PWD
-        set -l search_dir (pwd)
-        while true
-          for file in .node-version .nvmrc
-              if test -e $search_dir/$file
-                  nvm install --silent
-                  return
-              end
-          end
-
-          if [ $search_dir = "/" ]; or [ $search_dir = "" ]
-              nvm use default --silent
-              return
-          end
-
-          set search_dir (dirname $search_dir)
-        end
-    end
-
-    __nvm_auto
-end
-
-
-
 if not set -q JAVA_HOME; and /usr/libexec/java_home >/dev/null 2>/dev/null
     set -gx JAVA_HOME (/usr/libexec/java_home)
 end
 
 
-if not set -q GROOVY_HOME
-    if test -d /usr/local/opt/groovy/libexec
-        set_if_absent GROOVY_HOME /usr/local/opt/groovy/libexec
-    else if test -d /opt/homebrew/opt/groovy/libexec
-        set_if_absent GROOVY_HOME /opt/homebrew/opt/groovy/libexec
-    end
-end
-
-
 if not set -q RIPGREP_CONFIG_PATH; and test -r $XDG_CONFIG_HOME/ripgrep/ripgreprc
     set -gx RIPGREP_CONFIG_PATH $XDG_CONFIG_HOME/ripgrep/ripgreprc
-end
-
-
-if type -p pyenv >/dev/null
-    set -gx PYENV_ROOT $XDG_DATA_HOME/pyenv
-    fish_add_path $PYENV_ROOT/bin
-
-    if status is-login
-        pyenv init --path | source
-    end
-
-    if status is-interactive
-        pyenv init - | source
-    end
 end
 
 
